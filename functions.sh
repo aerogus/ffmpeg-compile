9a10fd5d2340b76569a7f695ddd36f576e0036c9
Guillaume Seznec <guillaume@seznec.fr>
2025-05-01 11:25:26 +0200

    hack autoconf 2.7

--- functions.sh
+++ functions.sh
@@ -81,7 +81,18 @@ installDependencies()
     if [[ "$OS" == "debian" ]]; then
         apt -y install curl bzip2 autoconf automake g++ cmake libtool pkg-config git-core
     elif [[ "$OS" == "almalinux" ]] || [[ "$OS" == "redhat" ]]; then
-        dnf -y install autoconf automake bzip2 bzip2-devel cmake gcc gcc-c++ git libtool make pkgconfig zlib-devel file
+        dnf -y install automake bzip2 bzip2-devel cmake gcc gcc-c++ git libtool make pkgconfig zlib-devel file
+        # Almalinux installe autoconf 2.6 mais nasm nécessite 2.71 mini :
+        dnf -y remove autoconf
+        dnf -y install autoconf2.7x
+        cd /usr/bin
+        # mais le package a installé des noms alternatifs
+        ln -s autoconf27 autoconf
+        ln -s autoheader27 autoheader
+        ln -s autom4te27 autom4te
+        ln -s autoreconf27 autoreconf
+        ln -s autoscan27 autoscan
+        ln -s autoupdate27 autoupdate
     elif [[ "$OS" == "centos" ]]; then
         # file pour ? optionnel ?
         # bzip2 pour décompresser les archives .tar.bz2
