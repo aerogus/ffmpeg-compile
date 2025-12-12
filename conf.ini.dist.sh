2536021b3689d97f7f26628c6335ddc360e2b222
Guillaume Seznec <guillaume@seznec.fr>
2025-12-12 09:32:51 +0100

    version 8.0.1

--- conf.ini.dist.sh
+++ conf.ini.dist.sh
@@ -25,4 +25,4 @@ export VERSION_X264="stable"
 export VERSION_X265="4.1" # https://bitbucket.org/multicoreware/x265_git
 export VERSION_FRIBIDI="1.0.16" # https://github.com/fribidi/fribidi/releases
 export VERSION_ASS="0.17.3" # https://github.com/libass/libass/releases
-export VERSION_FFMPEG="8.0" # https://ffmpeg.org/releases/
+export VERSION_FFMPEG="8.0.1" # https://ffmpeg.org/releases/
